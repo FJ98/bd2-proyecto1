5c5c3f6cabaf464320e22648c31294eafbf02a7f
FJ98 <felix.solano@utec.edu.pe>
2019-09-18 17:49:57 -0500

    Editando

--- main.cpp
+++ main.cpp
@@ -1,26 +1,38 @@
 #include <bits/stdc++.h>
 
+#include <utility>
+
 using namespace std;
 
 enum{ entero, flotante, doble, caracter };
 
 class Archivo{
 private:
-    int start; // Para saber donde empieza la data
+    int startData; // Para saber donde empieza la data
     int numeroDeCampos; // # de campos
     char** campo; // lista de campos
-    char** tipoDeDatoCampo; // lista de los tipos de los campos
-    char* fileName; // nombre del archivo
+    char** tipoDeDatoDeCampo; // lista de los tipos de los campos
+    std::string fileName; // nombre del archivo
 public:
-    void loadFromFile(char* filename);
+    Archivo() = default;
+    void loadFromFile(std::string);
     void showData();
 
 };
 
-void Archivo::loadFromFile(char* filename) {
-    this->fileName = filename;
+
+void Archivo::loadFromFile(std::string filename) {
+    this->fileName = std::move(filename);
     this->numeroDeCampos = 0;
 
+    fstream inFile;
+    inFile.open("PRUEBA2.bin", ios::in | ios::binary);
+    if (inFile.is_open()) {
+        int temp;
+        inFile.read((char *) &temp, sizeof(int));
+        cout << temp << endl;
+        inFile.close();
+    } else cout << "Could not open the index-file.\n";
 }
 void Archivo::showData() {
 
@@ -32,7 +44,14 @@ class HashFile{
 };
 
 
+
 int main() {
     std::cout << "Hello World" << std::endl;
+
+     auto archivo = new Archivo;
+     archivo->loadFromFile("prueba1.bin");
+
+
+
     return 0;
 }
